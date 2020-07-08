0cdcdf458194172e52f27a9eaf6559b773912e33
Chowdhury Muhammad Mehdi Hasan <mehdi.uiu@gmail.com>
2020-07-08 20:35:36 +0600

    Update 1test.R

--- ML-workbook/1test.R
+++ ML-workbook/1test.R
@@ -1,9 +1,11 @@
 # setwd("~/datasets/titanic")
 
-train <- read.csv('https://raw.githubusercontent.com/raqueeb/mltraining/master/ML-workbook/train.csv')
+
+
+train <- read.csv('https://github.com/mehdi2/mltraining/blob/master/ML-workbook/test.csv')
 # train <- read.csv("~/datasets/titanic/train.csv")
 # test <- read.csv("~/datasets/titanic/test.csv")
-test <- read.csv('https://raw.githubusercontent.com/raqueeb/mltraining/master/ML-workbook/test.csv')
+test <- read.csv('https://github.com/mehdi2/mltraining/blob/master/ML-workbook/test.csv')
 
 # লাইব্রেরি লোড করি - "rpart" হচ্ছে ডিসিশন ট্রি অ্যালগরিদম 
 # (rpart: Recursive Partitioning and Regression Trees)
